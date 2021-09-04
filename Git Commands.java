3666d626085867096da7a02ad2500395715824d4
Shahrukh <sharuk9511@gmail.com>
2021-09-04 13:55:32 +0530

    one file change

---
+++ Git Commands.java
@@ -0,0 +1,13 @@
+===Add the new file
+C:\Users\Shahrukh\Desktop\OK>git init
+C:\Users\Shahrukh\Desktop\OK>git add .
+C:\Users\Shahrukh\Desktop\OK>git commit -m "first commit"
+C:\Users\Shahrukh\Desktop\OK>git remote add origin https://github.com/sharuk95411/Program.git
+C:\Users\Shahrukh\Desktop\OK>git push -u origin master
+
+== Save the changes in directory 
+C:\Users\Shahrukh\Desktop\OK>git status // isse new bni file ka nme aa jyga an
+C:\Users\Shahrukh\Desktop\OK>git add .
+C:\Users\Shahrukh\Desktop\OK>git commit -m "One file Changes"
+C:\Users\Shahrukh\Desktop\OK>git push -u origin master
+C:\Users\Shahrukh\Desktop\OK>git status // isme kuch b ni ayag bcz new file ab add ho chuki h
