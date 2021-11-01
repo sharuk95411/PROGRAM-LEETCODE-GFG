4747f3ce456d28bed6f71d9fb02e5d94edfdf619
Shahrukh <sharuk9511@gmail.com>
2021-11-01 18:40:42 +0530

    11-01-21

---
+++ GFG/Searching And Sorting/First and last occurrences of x.java
@@ -0,0 +1,60 @@
+// Simple two function bna do first leftMost index k liye and second right most index k liye
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+     
+        int arr[]= new int[2];
+        arr[0]=startIndex(nums,target);
+     
+        arr[1]= endIndex(nums,target);
+       return arr;
+    }
+    
+     public int startIndex(int arr[],int target)
+   {
+       int index=-1;
+       int start=0;
+       int end= arr.length-1;
+       while(start<=end)
+       {
+           int mid= start+ (end-start)/2;
+           if(arr[mid]>=target)
+           {
+               end=mid-1;
+           }
+           else
+           {
+            start= mid+1;   
+           }
+           if(arr[mid]==target)
+           {
+               index=mid;
+           }
+       }
+    
+       return index;
+   }
+   public int endIndex(int arr[],int target)
+   {
+       int index=-1;
+       int start=0;
+       int end= arr.length-1;
+       while(start<=end)
+       {
+           int mid= start+ (end-start)/2;
+           if(arr[mid]<=target)
+           {
+               start=mid+1;
+           }
+           else
+           {
+            end= mid-1;   
+           }
+       
+       if(arr[mid]==target)
+       {
+           index= mid;
+       }
+       }
+       return index;
+   }
+}
--- Pending Question/Pending Questions.java
+++ Pending Question/Pending Questions.java
@@ -1,3 +1,4 @@
 1-Minimum Cost Tree From Leaf Values (Leetcode)
 2-Count occurences of a given word in a 2-d array (GFG)
 3-Find the string in grid
+4-Flatten a Multilevel Doubly Linked List
